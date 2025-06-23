4d989c206363425f0f3cdb575987ef82222416c1
maferdez <mifernandez@g.hmc.edu>
2025-06-23 16:31:24 -0700

    updated the main.py to draw from sys.argv for args

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from stats import get_number_words
 from stats import get_number_char
 from stats import sort_dict
+import sys
 def main():
-    book = get_book_text("/home/mafer/bootdevProjects/bookbot/books/frankenstein.txt")
+    if (len(sys.argv) != 2):#if our input command to the program doesnt have two arguments for program and filepath
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    book = sys.argv[1] #using the second element of the sys.argv list which should be the book path
+    book_text = get_book_text(book)
     print("============ BOOKBOT ============\n----------- Word Count ----------\n")
-    get_number_words(book)
+    get_number_words(book_text)
     print("--------- Character Count -------\n")
-    character_count = get_number_char(book)
+    character_count = get_number_char(book_text)
     sorted_dictionary_list = sort_dict(character_count)
     for i in range(0,len(sorted_dictionary_list)):
         if (sorted_dictionary_list[i]["char"].isalpha()):
